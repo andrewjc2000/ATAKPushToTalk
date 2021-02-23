9a42783ecba0ed0a5cc75ba285625968fafcc542
Andrew Chafos <andrewjchafos@gmail.com>
2021-02-23 03:59:11 -0500

    cleared all git history except for current code (basic button in plugin with start/stop popups on ATAK)

---
+++ README.md
@@ -0,0 +1,2 @@
+# ATAKPushToTalk
+A plugin to enable speech-to-text transcription for the Android Team Awareness Kit
---
+++ pushToTalk/app/src/test/java/com/atakmap/android/pushToTalk/ExampleTest.java
@@ -0,0 +1,13 @@
+
+package com.atakmap.android.pushToTalk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void basicArithmetic() {
+        assertEquals(4, 2 + 2);
+    }
+}
